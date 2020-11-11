d8e570fdc161f5e70165cc0c35fd2623b43fc15d
Matthew Shortt <shorttm@mac-3126.lan>
2020-11-11 16:29:02 -0500

    feat - add environment variables, styling

--- src/Summary.js
+++ src/Summary.js
@@ -1,207 +1,102 @@
-import britishOpen from './resources/Logo_of_The_Open_Championship.png';
-import React from 'react';
+import mastersTitle from './resources/masters_title.svg';
+import mastersLogo from './resources/masters_logo.png';
+import React, {useMemo} from 'react';
 
 export function Summary() {
-    const people = [
-        {
-            name: "Ronan Weir",
-            position: "1",
-            total: "-11",
-            players: [
-                {
-                    name: "Tony Finau",
-                    score: "-7"
-                },
-                {
-                    name: "Brooks Koepka",
-                    score: "-6"
-                },
-                {
-                    name: "Patrick Cantlay",
-                    score: "+2"
-                },
-                {
-                    name: "Paul Casey",
-                    score: "+4"
-                }
-            ]
-        },
-        {
-            name: "Kyle Gray",
-            position: "2",
-            total: "-7",
-            players: [
-                {
-                    name: "Rickie Fowler",
-                    score: "-5"
-                },
-                {
-                    name: "Danny Willett",
-                    score: "-5"
-                },
-                {
-                    name: "Dustin Johnson",
-                    score: "+3"
-                },
-                {
-                    name: "Tiger Woods",
-                    score: "+6(MC)"
-                }
-            ]
-        },
-        {
-            name: "Jack Vautour",
-            position: "3",
-            total: "-6",
-            players: [
-                {
-                    name: "Francesco Molinari",
-                    score: "-3"
-                },
-                {
-                    name: "Jon Rahm",
-                    score: "-2"
-                },
-                {
-                    name: "Jordan Spieth",
-                    score: "-1"
-                },
-                {
-                    name: "Louis Oosthuizen",
-                    score: "-1"
-                }
-            ]
-        },
-        {
-            name: "Tyler Verbanec",
-            position: "4",
-            total: "-5",
-            players: [
-                {
-                    name: "Tyrrell Hatton",
-                    score: "-5"
-                },
-                {
-                    name: "Lucas Bjerregaard",
-                    score: "-2"
-                },
-                {
-                    name: "Rory Mcilroy",
-                    score: "+2(MC)"
-                },
-                {
-                    name: "Gary Woodland",
-                    score: "+3(MC)"
-                },
-            ]
-        },
-        {
-            name: "Carter Brooks",
-            position: "5",
-            total: "-3",
-            players: [
-                {
-                    name: "Tommy Fleetwood",
-                    score: "-9"
-                },
-                {
-                    name: "Xander Schauffele",
-                    score: "+2"
-                },
-                {
-                    name: "Graeme McDowell",
-                    score: "+4"
-                },
-                {
-                    name: "Bryson Deschambeau",
-                    score: "+5(MC)"
-                }
-            ]
-        },
-        {
-            name: "Matt Shortt",
-            position: "6",
-            total: "E",
-            players: [
-                {
-                    name: "Justin Rose",
-                    score: "-1"
-                },
-                {
-                    name: "Henrik Stenson",
-                    score: "-1"
-                },
-                {
-                    name: "Eddie Pepperell",
-                    score: "+8"
-                },
-                {
-                    name: "Jason Day",
-                    score: "+2(MC)"
-                }
-            ]
-        },
-        {
-            name: "Chris Grills",
-            position: "7",
-            total: "+2",
-            players: [
-                {
-                    name: "Justin Thomas",
-                    score: "-3"
-                },
-                {
-                    name: "Matt Kuchar",
-                    score: "+2"
-                },
-                {
-                    name: "Hideki Matsuyama",
-                    score: "+3(MC)"
-                },
-                {
-                    name: "Adam Scott",
-                    score: "+9(MC)"
-                }
 
-            ]
-        }
-    ];
+    const WORST_SCORE_DAY_3 = useMemo(() => parseInt(process.env.REACT_APP_WORST_SCORE_DAY_3), []);
+    const WORST_SCORE_DAY_4 = useMemo(() => parseInt(process.env.REACT_APP_WORST_SCORE_DAY_4), []);
+    const IS_WEEKEND = useMemo(() => parseInt(process.env.REACT_APP_IS_WEEKEND), []);
+    const people = useMemo(() => getPeople(JSON.parse(process.env.REACT_APP_SCORES)), []);
+
     return (
         <div className="uk-height-viewport uk-background-default uk-margin-medium-bottom">
-            <div className="uk-container uk-text-center@m">
-                <h1 className="uk-heading-divider uk-margin-top">PGA Pool</h1>
-                <img src={britishOpen} width="100px" height="100px" alt="The British Open Logo"
-                     className="uk-position-small uk-position-top-right"/>
+            <div className="uk-container uk-text-center">
+                {/*<h1 className="uk-heading-divider uk-margin-top">PGA Pool</h1>*/}
+                <div className="uk-heading-divider uk-margin-top">
+                    <img src={mastersTitle} width="200px" alt="The British Open Logo"/>
+                </div>
+                <img src={mastersLogo} width="90px" height="100px" alt="The British Open Logo"
+                     className="uk-position-small uk-position-top-right uk-margin-top uk-visible@l"/>
                 <div className="uk-align-center uk-width-1-1@m">
-                    <div className="uk-text-center" data-uk-grid>
+                    <div className="uk-text-center" data-uk-grid data-uk-sortable>
                         {people.map((person, i) =>
                             <div key={i} className="uk-width-1-3@l uk-width-1-2@m">
                                 <div className="uk-card uk-card-default uk-card-body">
-                                    <div className="uk-card-badge uk-label">{person.total}</div>
+                                    <div className="uk-card-badge uk-label">{getNumberAsGolfString(person.total)}</div>
                                     <h3 className="uk-card-title uk-margin-remove-bottom">{person.name}</h3>
                                     <p className="uk-text-meta uk-margin-remove-top">
-                                        Position: {person.position}
+                                        Position: {i + 1}
                                     </p>
-                                    <hr className="uk-divider-icon"></hr>
+                                    <hr className="uk-divider-icon"/>
                                     <table className="uk-table uk-table-striped">
                                         <thead>
                                         </thead>
                                         <tbody>
-                                        {person.players.map((player, k) =>
+                                        {person.players.map(({name, score, cut}, k) =>
                                             <tr key={k}>
-                                                <td>{player.name}</td>
-                                                <td>{player.score}</td>
+                                                <td>{name}</td>
+                                                <td>{getPlayerScoreAsString(score, cut)}</td>
                                             </tr>
                                         )}
                                         </tbody>
                                     </table>
-                                    <hr></hr>
+                                    <hr/>
                                 </div>
                             </div>)
                         }
                     </div>
                 </div>
+                <div className={`uk-placeholder uk-text-left uk-padding-small ${!IS_WEEKEND ? 'uk-hidden' : ''}`}>
+                    <span id="missed-cut">
+                        <span className='uk-text-primary'>* </span>
+                        The player has missed the cut so the worst scores from day 3 and 4 have been added as a penalty:
+                    </span>
+                    <ul className="uk-list uk-list-disc uk-list-primary uk-text-small uk-margin-small-top">
+                        <li>Saturday's worst score: {getNumberAsGolfString(WORST_SCORE_DAY_3)}</li>
+                        <li>Sunday's worst score: {getNumberAsGolfString(WORST_SCORE_DAY_4)}</li>
+                    </ul>
+                </div>
+
             </div>
         </div>
     );
+
+    function getPeople(people = []) {
+        return people
+            .map(person => {
+                person.players
+                    .sort((playerA, playerB) => {
+                        if (!playerA.cut && playerB.cut) return -1;
+                        else if (playerA.cut && !playerB.cut) return 1;
+                        else return playerA.score - playerB.score;
+                    })
+                    .slice(0, 3)
+                    .map((player, i) => {
+                        const scoreNumber = parseInt(player.score);
+                        person.total += !player.cut ? scoreNumber : getMissedCutScore(scoreNumber);
+                        return player;
+                    })
+                return person
+            })
+            .sort((personA, personB) => personA.total - personB.total);
+    }
+
+    function getPlayerScoreAsString(score, isCut) {
+        let scoreNumber = parseInt(score);
+        return (isCut
+            ? <span>{getNumberAsGolfString(getMissedCutScore(scoreNumber))} <a href='#missed-cut' data-uk-scroll>(MC)*</a></span>
+            : getNumberAsGolfString(scoreNumber));
+    }
+
+    function getMissedCutScore(score) {
+        return score + WORST_SCORE_DAY_3 + WORST_SCORE_DAY_4
+    }
+
+}
+
+function getNumberAsGolfString(total) {
+    if (total === 0) return 'E';
+    else if (total > 0) return `+${total}`;
+    else return total;
 }
